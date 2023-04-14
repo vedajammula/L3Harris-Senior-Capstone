18a2639c35043fb0e0beac516ba42bcb32f48d80
ssarthak01 <sash9861@colorado.edu>
2023-04-14 15:23:37 -0600

    Working KNN w/ unsupervised learning

--- LSTM_model/Data_Manipulations/KNN_unsupervised.py
+++ LSTM_model/Data_Manipulations/KNN_unsupervised.py
@@ -3,61 +3,83 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
+from scipy.spatial import distance
 #source -> https://www.kaggle.com/code/kimchanyoung/simple-anomaly-detection-using-unsupervised-knn 
 
-class KNN_unsupervised:
-
-    def __init__(self, df):
-        self.df = df
 
 
-    def process_data(self):
-        date_indexed = []
-        #self.df['Date'] = pd.to_datetime(self.df['Date'])
-        for i in range(len(self.df)):
-            date_indexed.append(i)
-        new_df = pd.DataFrame(data={'Date': date_indexed, 'Close': self.df['Close']})
-        self.df = new_df
+class KNN_unsupervised():
+
+    def __init__(self, filename, start_date, end_date):
+        self.filename = filename
+        self.start_date = start_date
+        self.end_date = end_date
     
-    def run_KNN(self):
-        self.process_data()
-        #create model
-        neighbors = NearestNeighbors(n_neighbors = math.ceil(math.sqrt(len(self.df))))
-        #fit model
-        neighbors.fit(self.df)
-        #get distances and indices of neighbors
-        dists, indices = neighbors.kneighbors(self.df)
-        #plot 
-        plt.figure(figsize=(15, 7))
-        plt.plot(dists.mean(axis =1))
-        #get distance mean
-        distances = pd.DataFrame(dists)
-        distances_mean = distances.mean(axis =1)
-        #get abnormal points
-        quantile_75 = distances_mean.quantile(.75)
-        threshold = math.ceil(quantile_75)
-        outlier_index = np.where(distances_mean > threshold)
-        outlier_vals = self.df.iloc[outlier_index]
+    def process_data(self):
+        #read csv file and set the dates to what we want 
+        df_temp = pd.read_csv('../../stock_data/'+self.filename, usecols=['Date', 'Close'], na_values=['nan'])
+        df_temp['Date'] = pd.to_datetime(df_temp['Date'])
 
-        #plot outliers
-        plt.figure(figsize=(20, 7))
-        plt.plot(self.df["Date"], self.df["Close"], color = "b")
-        # plot outlier values
-        plt.scatter(outlier_vals["Date"], outlier_vals["Close"], color = "r")
+        df = df_temp[(df_temp['Date'] >= self.start_date) & (df_temp['Date'] <= self.end_date)]
+        df.set_index('Date', inplace=True)
+
+        return df
+
+
+    def run_KNN(self):
+        df = self.process_data()
+        #compute percent change in stock of close price 
+        #delta t = 100 * ((x_t - x_(t-1))/ x_(t-1))
+        N,d = df.shape
+        delta = pd.DataFrame(100*np.divide(df.iloc[1:,:].values-df.iloc[:N-1,:].values, df.iloc[:N-1,:].values), columns=df.columns, index=df.iloc[1:].index)
+
+        #KNN algo with nearest neighbors
+        knn = 4
+        nbrs = NearestNeighbors(n_neighbors=knn, metric=distance.euclidean).fit(delta.values)
+        distances, indices = nbrs.kneighbors(delta.values)
+        graphing_dates = list(df.index)
+        graphing_dates.pop(0)
+        anomaly_score = distances[:,knn-1]
+
+        fig = plt.figure(figsize=(10,6))
+        ax = fig.add_subplot(111)
+        ax.set_title('Colormap of outliers')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Anomaly Score of Close Values')
+        p = ax.scatter(graphing_dates ,list(delta.Close),c=anomaly_score,cmap='jet')
+
+        fig.colorbar(p)
         plt.show()
 
-        return outlier_vals
+        #calculate and append anomaly scores 
+        anom = pd.DataFrame(anomaly_score, index=delta.index, columns=['Anomaly_Score'])
+        result = pd.concat((delta,anom), axis=1)
+
+        #this is a dataframe should display to UI
+        largest_anomalies = result.nlargest(5,'Anomaly_Score')
+        
+        threshold = largest_anomalies['Anomaly_Score'].min()
+        indices = (result.index[result['Anomaly_Score'] >= threshold]).tolist()
+        indices_closevals = [df.loc[ind, 'Close'] for ind in indices]
+
+        for i in range(len(indices)):
+            df = df.drop(indices[i])
+        plt.title(self.filename + ' data visualization ' + 'outliers')
+        plt.plot(df.index, df['Close'])
+        plt.scatter(indices, indices_closevals, color = "r")
+        plt.xlabel('Time')
+        plt.ylabel('Close Price')
+        plt.show()
+
+        return df
 
 
-df1 = pd.DataFrame(index= pd.date_range('2010-01-04','2017-01-03',freq='B'))
-df_temp = pd.read_csv('../../stock_data/nasdaq_all.csv', index_col='Date', parse_dates=True, usecols=['Date', 'Close'], na_values=['nan'])
-df1 = df1.join(df_temp)
 
-# df1['Date'] = pd.to_datetime(df1['Date'])
-df = df1.dropna()
-#df.rename(['Date', 'Close'], axis='columns')
+########### To run and test
+# filename = 'nasdaq_all.csv'
+# start_date = '2010-01-04'
+# end_date = '2017-01-03'
 
-#print(df.head())
-
-temp = KNN_unsupervised(df)
-temp.run_KNN()
+# k = KNN_unsupervised(filename, start_date, end_date)
+# manipulated_data = k.run_KNN()
+# print(manipulated_data)
