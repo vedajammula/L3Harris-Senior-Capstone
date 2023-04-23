1c28f0749d6f0f21aeb42785c2e2f85d5e3537cc
ItsMe1324 <AndrewWu1324@gmail.com>
2023-04-22 20:01:58 -0600

    Window self

--- LSTM_model/Window.py
+++ LSTM_model/Window.py
@@ -11,19 +11,19 @@ class Window():
         self.size = len(self.df.index)
         #database, window size, and step size and index
     
-    def numberOfWindows():
+    def numberOfWindows(self):
         return math.ceil((self.size-wSize) / self.stepSize)
 
-    def nextWindow():
+    def nextWindow(self):
         limit = self.index + self.wSize + self.stepSize if self.index + self.wSize + self.stepSize <= self.size else self.size
         win = self.df.iloc[self.index : self.index + wSize]
         judge = self.df.iloc[self.index + wSize : limit]
         self.index = self.index + self.stepSize
         return (win, judge)
 
-    def accepted(dataframe):
+    def accepted(self, dataframe):
         cols = list(self.df.columns) 
         self.df.loc[self.df.date.isin(dataframe.date), cols] = dataframe[cols].values
 
-    def cleaned():
+    def cleaned(self):
         return self.df
