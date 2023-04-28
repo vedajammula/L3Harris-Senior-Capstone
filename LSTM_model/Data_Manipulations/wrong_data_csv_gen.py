421647a9b6d05261704fa207c98d3e37f49a2462
portiabh1 <portiabh1@gmail.com>
2023-04-27 19:28:41 -0600

    wrong data gen py file

---
+++ LSTM_model/Data_Manipulations/wrong_data_csv_gen.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import random
+import numpy as np
+import csv
+import pandas as pd
+import copy
+
+#import os 
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
+# cwd = os.getcwd()
+# print(cwd)
+
+dj_df = pd.read_csv("../../stock_data/djia_2012.csv") 
+# nas_df = pd.read_csv("stock_data/nasdaq_all.csv") 
+# rus_df = pd.read_csv("stock_data/russel2000_all.csv") 
+
+# Grab up to 10 random intervals of (randomly generated) n lines. Using Gaussian noise to make data in intervals progressively get worse. 
+def gen_wrong(sample):
+    df = pd.read_csv(sample) #get data col - can use iloc[:,0] to get Date col if needed
+    df_copy = df.copy() #make copy of dataframe to apply change
+    avg_length_crash = 342 #average length of stock market crash
+    n = random.randint(5, 10) #num intervals
+    noise_levels = [10, 20, 30, 40, 50] #for noise
+    for i in range(n):
+        num_rows = random.randint(1, avg_length_crash) #generate random number of rows for interval
+        start_idx = random.randint(0, len(df) - num_rows) #start idx for intervall
+        end_idx = start_idx + num_rows #last idx for interval
+        interval = df_copy.iloc[start_idx:end_idx] #create interval
+        for j in range(len(noise_levels)): #apply noise to copy of df
+            noise = np.random.normal(0, noise_levels[j], num_rows) #generate random amt of noise
+            interval.iloc[:, 1] += noise #add noise
+            df_copy.iloc[start_idx:end_idx] = interval #add interval to df copy
+    return df_copy
+
+#testing
+new_djdata = gen_wrong("../../stock_data/djia_2012.csv")
+dj_df.compare(new_djdata)
+new_djdata.to_csv('new_djdata.csv', encoding= 'utf-8', index=False)
+
+# print(df.columns)
+# print(df_copy.columns)
+
+
+# In[10]:
+
+
+import random
+import numpy as np
+import csv
+import pandas as pd
+import copy
+
+#import os 
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
+# cwd = os.getcwd()
+# print(cwd)
+
+nasdaq_df = pd.read_csv("../../stock_data/nasdaq_all.csv") 
+# nas_df = pd.read_csv("stock_data/nasdaq_all.csv") 
+# rus_df = pd.read_csv("stock_data/russel2000_all.csv") 
+
+# Grab up to 10 random intervals of (randomly generated) n lines. Using Gaussian noise to make data in intervals progressively get worse. 
+def gen_wrong(sample):
+    df = pd.read_csv(sample) #get data col - can use iloc[:,0] to get Date col if needed
+    df_copy = df.copy() #make copy of dataframe to apply change
+    avg_length_crash = 342 #average length of stock market crash
+    n = random.randint(5, 30) #num intervals
+    noise_levels = [10, 20, 30, 40, 50] #for noise
+    for i in range(n):
+        num_rows = random.randint(1, avg_length_crash) #generate random number of rows for interval
+        start_idx = random.randint(0, len(df) - num_rows) #start idx for intervall
+        end_idx = start_idx + num_rows #last idx for interval
+        interval = df_copy.iloc[start_idx:end_idx] #create interval
+        print(interval)
+        for j in range(len(noise_levels)): #apply noise to copy of df
+            noise = np.random.normal(0, noise_levels[j], num_rows) #generate random amt of noise
+            print(noise)
+            interval.iloc[:, 6] += noise #add noise
+            df_copy.iloc[start_idx:end_idx] = interval #add interval to df copy
+    return df_copy
+
+
+new_nasdaqdata = gen_wrong("../../stock_data/nasdaq_all.csv")
+nasdaq_df.compare(new_nasdaqdata)
+new_nasdaqdata.to_csv('new_nasdaq.csv', encoding= 'utf-8', index=False)
+
+
+# In[12]:
+
+
+import random
+import numpy as np
+import csv
+import pandas as pd
+import copy
+
+#import os 
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(dir_path)
+# cwd = os.getcwd()
+# print(cwd)
+
+russel_df = pd.read_csv("../../stock_data/russel2000_all.csv") 
+# nas_df = pd.read_csv("stock_data/nasdaq_all.csv") 
+# rus_df = pd.read_csv("stock_data/russel2000_all.csv") 
+
+# Grab up to 10 random intervals of (randomly generated) n lines. Using Gaussian noise to make data in intervals progressively get worse. 
+def gen_wrong(sample):
+    df = pd.read_csv(sample) #get data col - can use iloc[:,0] to get Date col if needed
+    df_copy = df.copy() #make copy of dataframe to apply change
+    avg_length_crash = 342 #average length of stock market crash
+    n = random.randint(5, 30) #num intervals
+    noise_levels = [10, 20, 30, 40, 50] #for noise
+    for i in range(n):
+        num_rows = random.randint(1, avg_length_crash) #generate random number of rows for interval
+        start_idx = random.randint(0, len(df) - num_rows) #start idx for intervall
+        end_idx = start_idx + num_rows #last idx for interval
+        interval = df_copy.iloc[start_idx:end_idx] #create interval
+        print(interval)
+        for j in range(len(noise_levels)): #apply noise to copy of df
+            noise = np.random.normal(0, noise_levels[j], num_rows) #generate random amt of noise
+            print(noise)
+            interval.iloc[:, 6] += noise #add noise
+            df_copy.iloc[start_idx:end_idx] = interval #add interval to df copy
+    return df_copy
+
+
+new_russeldata = gen_wrong("../../stock_data/russel2000_all.csv")
+russel_df.compare(new_russeldata)
+new_russeldata.to_csv('new_russel.csv', encoding= 'utf-8', index=False)
+
+
+# In[ ]:
+
+
+
+
