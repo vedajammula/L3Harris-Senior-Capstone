e1751b89c2a037fcd2fab376b2dba7576bc31775
ssarthak01 <sash9861@colorado.edu>
2023-05-08 15:05:59 -0600

    Reduced pages from 6->3 and debugged errors

--- LSTM_model/pages/2_📈_DJIA.py
+++ LSTM_model/pages/2_📈_DJIA.py
@@ -4,13 +4,24 @@ from Pipeline import Pipeline
 st.set_page_config(page_title="DJIA", page_icon="📈")
 
 st.markdown("# DJIA")
-st.sidebar.header("DJIA")
+tab1, tab2 = st.tabs(["Real DJIA Data", "Manipulated DJIA Data"])
 
 filename = 'djia_2012.csv'
 start_date = '2013-01-03'
-end_date = '2019-01-02'
+end_date = '2020-01-02'
+data_flag = 0
 
-pipeline = Pipeline(filename, start_date, end_date)
-pipeline.run_pipeline()
+pipeline = Pipeline(filename, start_date, end_date, data_flag)
+
+with tab1:
+    pipeline.run_pipeline()
+
+filename = 'new_djdata.csv'
+data_flag = 1
+pipeline = Pipeline(filename, start_date, end_date, data_flag)
+
+
+with tab2:
+    pipeline.run_pipeline()
 
 # st.write('IN PROGRESS')
--- LSTM_model/pages/3_📈_RUSSELL2000.py
+++ LSTM_model/pages/3_📈_RUSSELL2000.py
@@ -4,11 +4,22 @@ from Pipeline import Pipeline
 st.set_page_config(page_title="RUSSELL 2000", page_icon="📈")
 
 st.markdown("# RUSSELL 2000")
-st.sidebar.header("RUSSELL 2000")
+tab1, tab2 = st.tabs(["Real RUSSELL2000 Data", "Manipulated RUSSELL2000 Data"])
 
 filename = 'russel2000_all.csv'
 start_date = '2010-01-04'
 end_date = '2017-01-03'
+data_flag = 0
 
-pipeline = Pipeline(filename, start_date, end_date)
-pipeline.run_pipeline()
+pipeline = Pipeline(filename, start_date, end_date, data_flag)
+
+with tab1:
+    pipeline.run_pipeline()
+
+filename = 'new_russel.csv'
+data_flag = 1
+pipeline = Pipeline(filename, start_date, end_date, data_flag)
+
+
+with tab2:
+    pipeline.run_pipeline()
